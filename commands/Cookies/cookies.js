0579178f70d00b5172658d9e27dd4f2aaa4242f6
Paultje52 <paultje52@gmail.com>
2020-05-30 18:21:24 +0200

    Cookies command helemaal klaar!

--- commands/Cookies/cookies.js
+++ commands/Cookies/cookies.js
@@ -4,28 +4,28 @@ module.exports = class Cookies extends require(`${process.cwd()}/util/command.js
 			name: "cookies",
 			description: "Kijk hoeveel cookies je hebt!",
 			dir: __dirname,
-			alias: ["p"]
+			alias: ["koek", "koekjes"]
 		}, {}, {
 			examples: [
-				"<cmd> cookies",
-				"<cmd> cookies <@327462385361092621>"
+				"<cmd>",
+				"<cmd> <@327462385361092621>"
 			]
 		});
 	}
 
 	async run(message, args) {
-		if (!args[0]) return message.channel.send(message.embed()
+		if (!args[0]) return message.channel.send(message.embed(false)
 			.setTitle("Cookies")
-			.setDescription(`Je hebt ${message.author.settings.cookies.amount} koekjes!\nJe bent ${message.author.settings.cookies.cookieMasterCount} keer cookiemaster geweest!`)	
-		);
+			.setDescription(`Je hebt **${message.author.settings.cookies.amount} koekjes**!\nJe bent **${message.author.settings.cookies.cookieMasterCount} keer** cookiemaster geweest!`)	
+			);
     
     let member = message.getMember(args[0]);
 		if (!member) return message.error(`Gebruiker ${args[0]} niet gevonden!`);
 		if (!member.user.settings) member.user.settings = await this.client.db.get(`author-${member.id}`);
 		if (!member.user.settings) member.user.settings = this.client.config.authorSettings;
-		message.channel.send(message.embed()
+		message.channel.send(message.embed(false)
 			.setTitle(`Cookies van ${member.displayName}`)
-			.setDescription(`${member.displayName} heeft ${message.author.settings.cookies.amount} koekjes!\n${member.displayName} is ${message.author.settings.cookies.cookieMasterCount} keer cookiemaster geweest!`)	
+			.setDescription(`${member} heeft **${message.author.settings.cookies.amount} koekjes**!\n${member} is **${message.author.settings.cookies.cookieMasterCount} keer** cookiemaster geweest!`)	
 		);
 	}
 }
